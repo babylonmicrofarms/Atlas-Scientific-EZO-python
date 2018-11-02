76461bae3a0685cb9b1d6736363ed2d62b1cd4f0
Sam Korn <korn94sam@gmail.com>
2018-11-02 17:34:38 -0400

    utf-8 instead of ascii- build-revert

--- Atlas_Sensors/smbus.py
+++ Atlas_Sensors/smbus.py
@@ -51,7 +51,8 @@ class AtlasSMBus:
 		if response_code == 1:
 			self.logger.debug("AtlasSMBus Command Successful!")
 			# Slice the buffer returned from the read, convert to ascii, and remove null characters
-			result = read.buf[1:31].decode('UTF-8').rstrip('\x00')
+			print(read.buf)
+			result = read.buf[1:16].decode('ascii').rstrip('\x00')
 			return result
 		elif response_code != 1:
 			# Return to the user the error code if error occured
