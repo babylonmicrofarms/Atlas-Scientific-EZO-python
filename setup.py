a7bad9cbefa24f5cdff298de6f3983a21a43670f
Sam Korn <korn94sam@gmail.com>
2018-11-06 13:33:23 -0500

    More bits

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='Atlas_Sensors',
-    version='0.2.1dev',
+    version='0.2.2dev',
     packages=['Atlas_Sensors'],
     license='MIT',
     author='Sam Korn',
